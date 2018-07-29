26ef103b462637101981f8e5fc9e81a91fb07bf9
Jamie Strachan <jstrachan@infotech.com>
2018-07-28 23:17:12 -0400

    got the game working, although the code should be refactored

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -1,13 +1,60 @@
 class GameOfLife
-  attr_accessor :state
-
   def initialize(start_state = '')
     @state = start_state
     @board = start_state.split('\n')
-    @rows, @cols = @board.shift.split(' ')
+    @rows, @cols = @board.shift.split(' ').map(&:to_i)
+    @board = @board.map { |r| r.split('') }
+  end
+
+  def state
+    ["#{@rows} #{@cols}", @board.map { |r| r.join('') }].join('\\n')
   end
 
   def tick
-    @state = @state.gsub(/\*/, '.')
+    @board = @board.each_with_index.map do |row, row_index|
+      row.each_with_index.map do |current, col_index|
+        age(current, row_index, col_index)
+      end
+    end
+  end
+
+  def age(current, row_index, col_index)
+    neighbors = determine_neighbors(row_index, col_index)
+    neighbor_cells = count_neighbor_cells(neighbors)
+    new_state = current
+    if current == '*'
+      if neighbor_cells < 2 || neighbor_cells > 3
+        new_state = '.'
+      end
+    else
+      if neighbor_cells == 3
+        new_state = '*'
+      end
+    end
+    new_state
+  end
+
+  def determine_neighbors(row_index, col_index)
+    retval = []
+    if row_index - 1 >= 0
+      retval.push([row_index - 1, col_index - 1]) if col_index - 1 >= 0
+      retval.push([row_index - 1, col_index])
+      retval.push([row_index - 1, col_index + 1]) if col_index + 1 < @cols
+    end
+
+    retval.push([row_index, col_index - 1]) if col_index - 1 >= 0
+    retval.push([row_index, col_index + 1]) if col_index + 1 < @cols
+
+    if row_index + 1 < @rows
+      retval.push([row_index + 1, col_index - 1]) if col_index - 1 >= 0
+      retval.push([row_index + 1, col_index])
+      retval.push([row_index + 1, col_index + 1]) if col_index + 1 < @cols
+    end
+
+    retval
+  end
+
+  def count_neighbor_cells(neighbors)
+    neighbors.select{ |row_index, col_index| @board[row_index][col_index] == '*' }.size
   end
 end
--- spec/gameoflife_spec.rb
+++ spec/gameoflife_spec.rb
@@ -1,13 +1,6 @@
 require 'gameoflife'
 
 RSpec.describe GameOfLife do
-  context 'with a board of dimensions 0, 0' do
-    it 'should return a board of dimensions 0, 0' do
-      gol = GameOfLife.new('0 0')
-      expect(gol.state).to eq '0 0'
-    end
-  end
-
   context 'with a board of dimensions 1, 1 and no cells' do
     it 'should return the same state' do
       gol = GameOfLife.new('1 1\n.')
@@ -34,11 +27,60 @@ RSpec.describe GameOfLife do
     end
   end
 
+  context 'determine_neighbors' do
+    it 'should work in the top left corner' do
+      gol = GameOfLife.new('4 4\n....\n....\n....\n....')
+      expect(gol.determine_neighbors(0, 0)).to eq [[0, 1], [1, 0], [1, 1]]
+    end
+
+    it 'should work on the right side' do
+      gol = GameOfLife.new('4 4\n....\n....\n....\n....')
+      expect(gol.determine_neighbors(2, 3)).to eq [[1, 2], [1, 3], [2, 2], [3, 2], [3, 3]]
+    end
+
+    it 'should work in the middle' do
+      gol = GameOfLife.new('4 4\n....\n....\n....\n....')
+      expect(gol.determine_neighbors(1, 1)).to eq [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]]
+    end
+
+    it 'should work in the bottom right corner' do
+      gol = GameOfLife.new('4 4\n....\n....\n....\n....')
+      expect(gol.determine_neighbors(3, 3)).to eq [[2, 2], [2, 3], [3, 2]]
+    end
+  end
+
+  context 'count_neighbor_cells' do
+    it 'should count properly for few neighbors, no cells' do
+      gol = GameOfLife.new('2 2\n..\n..')
+      expect(gol.count_neighbor_cells([[0, 1], [1, 0], [1, 1]])).to eq 0
+    end
+    it 'should count properly for few neighbors, many cells' do
+      gol = GameOfLife.new('2 2\n.*\n**')
+      expect(gol.count_neighbor_cells([[0, 1], [1, 0], [1, 1]])).to eq 3
+    end
+    it 'should count properly for many neighbors, many cells' do
+      gol = GameOfLife.new('3 3\n***\n***\n***')
+      expect(gol.count_neighbor_cells([[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]])).to eq 8
+    end
+    it 'should count properly for many neighbors, few cells' do
+      gol = GameOfLife.new('3 3\n*..\n*..\n*..')
+      expect(gol.count_neighbor_cells([[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 3]])).to eq 3
+    end
+  end
+
   context 'with a 2,2 board' do
     it 'should grow a new cell when there are three to start' do
       gol = GameOfLife.new('2 2\n.*\n**')
       gol.tick
       expect(gol.state).to eq '2 2\n**\n**'
+    end
+  end
+
+  context 'small exploder' do
+    it 'should reach a stable state' do
+      gol = GameOfLife.new('13 13\n.............\n.............\n.............\n.............\n.............\n......*......\n.....***.....\n.....*.*.....\n......*......\n.............\n.............\n.............\n.............')
+      16.times { gol.tick }
+      expect(gol.state).to eq '13 13\n......*......\n.....*.*.....\n.....*.*.....\n......*......\n.............\n.**.......**.\n*..*.....*..*\n.**.......**.\n.............\n......*......\n.....*.*.....\n.....*.*.....\n......*......'
     end
   end
 end
